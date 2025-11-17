4b04d59ec6a32efb7dbe660881b2b9e1d20daea0
N4YuC4 <yucelcan028@gmail.com>
2025-11-18 01:50:44 +0300

    host 0.0.0.0

--- app.py
+++ app.py
@@ -126,4 +126,4 @@ def static_general(filename):
     return send_from_directory(app.static_folder, filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
