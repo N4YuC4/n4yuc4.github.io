e0ddf051182db817ed244d125511f44bfcb99d51
N4YuC4 <yucelcan028@gmail.com>
2025-12-26 07:12:07 +0300

    Add XML schema definition to sitemap for strict validation

--- build.py
+++ build.py
@@ -106,7 +106,7 @@ def generate_sitemap(pages):
     print("Sitemap.xml oluşturuluyor...")
     
     sitemap_content = ['<?xml version="1.0" encoding="UTF-8"?>']
-    sitemap_content.append('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">')
+    sitemap_content.append('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">')
     
     today = datetime.date.today().isoformat()
     
